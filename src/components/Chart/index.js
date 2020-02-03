62ab01a99b35c171384a6a3ec961fc0de6d1c31b
akrm19 <akrm19@gmail.com>
2020-02-03 17:05:33 -0600

    Add custom chart component Add instance of Char to App.js

---
+++ src/components/Chart/index.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const Chart = (props) => {
+  const isEmpty  = !props.data || props.data.length === 0;
+  const svgRef = useRef(null);
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  const chartHeight = props.height || 200;
+  const chartWidth = props.height || 200;
+  const radius = Math.min(chartHeight, chartWidth) / 2;
+  const pie = d3.pie();
+  const arc = d3.arc()  
+    .innerRadius(0)
+    .outerRadius(radius);
+
+  useEffect(() => {
+    if(isEmpty) return;
+
+    let svg = d3.select(svgRef.current);
+    let group = svg
+      .append('g')
+      .attr('transform', 'translate(' + chartWidth / 2 +  ',' + chartHeight / 2 + ')');
+    let arcGroups = group.selectAll('path')
+      .data(pie(props.data ))
+      .enter()
+        .append('path')
+        // .attr('class', 'arc')
+        .attr('fill', function (d,i) {
+          return color(i);
+        })
+        .attr('d', arc);
+  }, []);
+
+
+  return ( 
+    <div>
+      <svg height={chartHeight} width={chartWidth} ref={svgRef}/>
+    </div>
+   );
+}
+ 
+export default Chart;
