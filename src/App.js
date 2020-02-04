1366f27000a01714ea978164d10b160af5c0e56c
akrm19 <akrm19@gmail.com>
2020-02-04 12:02:41 -0600

    Add more sample data in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PieChart from './components/PieChart';
 import LegislatorGenderPieChart from '../src/components/LegislatorsGenderPieChart';
-import SampleSenateMembers from '../src/assets/samples/legislators_senate';
+import SampleSenateResult from '../src/assets/samples/legislators_senate';
+import SampleCongressResult from '../src/assets/samples/legislators_congress';
 
 function App() {
+  const sampleSenateMembers = SampleSenateResult.results[0].members;
+  const sampleCongressMembers = SampleCongressResult.results[0].members;
   const testData = [
     {label: 'lab1', value: 9},
     {label: 'labelFor8', value: 8},
     {label: 'labelFor7', value: 7},
     {label: 'labelFor6', value: 1},];
-  console.log(SampleSenateMembers);
+  // console.log(SampleSenateResult);
   return (
     <div className="App">
       <header className="App-header">
-      <PieChart data={testData} />
-      <LegislatorGenderPieChart legislators={SampleSenateMembers.results[0].members} />
+        <LegislatorGenderPieChart legislators={sampleSenateMembers} />
+        <LegislatorGenderPieChart legislators={sampleCongressMembers} />
+        <LegislatorGenderPieChart legislators={[...sampleCongressMembers, ...sampleSenateMembers]} />
+
+
 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
