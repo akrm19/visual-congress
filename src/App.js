50960c972ab3e281a41fa85034de64c7a4b8dc8b
akrm19 <akrm19@gmail.com>
2020-02-03 23:39:46 -0600

    -Add LegislatorGenderPieChart, which extracts gender data rom a list of legislators and creates a pie chart -Add sample LegislatorGenderPieChart to app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PieChart from './components/PieChart';
+import LegislatorGenderPieChart from '../src/components/LegislatorsGenderPieChart';
 import SampleSenateMembers from '../src/assets/samples/legislators_senate';
 
 function App() {
-  const testData = [9, 8, 7, 6, 1];
+  const testData = [
+    {label: 'lab1', value: 9},
+    {label: 'labelFor8', value: 8},
+    {label: 'labelFor7', value: 7},
+    {label: 'labelFor6', value: 1},];
+  console.log(SampleSenateMembers);
   return (
     <div className="App">
       <header className="App-header">
       <PieChart data={testData} />
+      <LegislatorGenderPieChart legislators={SampleSenateMembers.results[0].members} />
 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/LegislatorsGenderPieChart/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import PieChart from '../PieChart';
+
+const getLegislatorsGenderData = (legislators) => {
+  let results = [];
+  const genderData = legislators.reduce((obj, legislator) => {
+    const gender = legislator.gender ? legislator.gender.toLowerCase() : null
+    if(!gender) {
+      obj.notAvailable++;
+    } else if (gender === 'm') {
+      obj.male++;
+    } else if (gender === 'f')
+      obj.female++;
+    else {
+      if(!obj[gender])
+        obj[gender] = 0;
+      obj[gender]++;
+    }
+    return obj;
+  }, { male: 0, female: 0, notAvailable:0 });
+
+  for(let [key, value] of Object.entries(genderData)) {
+    results.push({label: key, value: value});
+  }
+  return results;
+}
+
+const LegislatorGenderPieChart = (props) => {
+  const isEmpty = !props.legislators || props.legislators.length === 0;
+  console.log(`Legislator data is empty: ${isEmpty}`);
+  let result = getLegislatorsGenderData(props.legislators);
+
+  return ( 
+    <div>
+      <PieChart data={result} />
+    </div>
+   );
+}
+ 
+export default LegislatorGenderPieChart;
