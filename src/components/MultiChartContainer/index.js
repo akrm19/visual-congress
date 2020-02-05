bc4a9faf291e4b955fcc44e30f8c3baf62eecb0b
akrm19 <akrm19@gmail.com>
2020-02-04 22:31:11 -0600

    Add padding for MultiCharContainer

--- src/components/MultiChartContainer/index.js
+++ src/components/MultiChartContainer/index.js
@@ -5,6 +5,9 @@ const MultiCharContainer = ({ children, title }) => {
   const ChartParentContainer = styled.div`
     display: flex;
     flex-direction: column;
+    margin-top: .5em;
+    margin-bottom: .5em;
+    /* background: rgba(255,255,255,0.5); */
   `;
   const CharContainer = styled.div`
     display: flex;
