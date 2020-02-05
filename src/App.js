8ed152e4f0d70f748cd23413ca6b13cfc6a1845a
akrm19 <akrm19@gmail.com>
2020-02-04 22:31:49 -0600

    -Udpate background photo -Reduce congress nums to last 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h2>Congress Visualized</h2>
       <GenderByCongressNumber congressNumber={116} title={'Current Congress (116th) by Gender'} />
-      { Array.from(Array(9)).map((val, idx, arr) => 
+      { Array.from(Array(4)).map((val, idx, arr) => 
         <GenderByCongressNumber key={idx} congressNumber={116 - idx - 1} />
       )}
     </div>
