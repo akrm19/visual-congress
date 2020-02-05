c5ee395ba6d1591423597d66d6e729feed932cbf
akrm19 <akrm19@gmail.com>
2020-02-04 21:43:18 -0600

    Upda App.js to show the gender data for the last 10 congresses

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import './App.css';
-import MultiCharContainer from '../src/components/MultiChartContainer';
-import LegislatorGenderPieChart from '../src/components/LegislatorsGenderPieChart';
-import SampleSenateResult from '../src/assets/samples/legislators_senate';
-import SampleCongressResult from '../src/assets/samples/legislators_congress';
+import GenderByCongressNumber from '../src/components/GenderByCongressNum';
 
 function App() {
-  const sampleSenateMembers = SampleSenateResult.results[0].members;
-  const sampleCongressMembers = SampleCongressResult.results[0].members;
-  const testData = [
-    {label: 'lab1', value: 9},
-    {label: 'labelFor8', value: 8},
-    {label: 'labelFor7', value: 7},
-    {label: 'labelFor6', value: 1},];
-  // console.log(SampleSenateResult);
   return (
     <div className="App">
       <h2>Congress Visualized</h2>
-        <MultiCharContainer title={'Current Congress (116th) by Gender'}>
-        <LegislatorGenderPieChart title={'Senate'} legislators={sampleSenateMembers}  />
-        <LegislatorGenderPieChart title={'Congress'} legislators={sampleCongressMembers}  />
-        <LegislatorGenderPieChart title={'Joined'} legislators={[...sampleCongressMembers, ...sampleSenateMembers]} />
-      </MultiCharContainer>
-      <header className="App-header">
-        <LegislatorGenderPieChart legislators={sampleSenateMembers} title={'Senate'} />
-        <LegislatorGenderPieChart legislators={sampleCongressMembers} />
-        <LegislatorGenderPieChart legislators={[...sampleCongressMembers, ...sampleSenateMembers]} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <GenderByCongressNumber congressNumber={116} title={'Current Congress (116th) by Gender'} />
+      { Array.from(Array(9)).map((val, idx, arr) => 
+        <GenderByCongressNumber key={idx} congressNumber={116 - idx - 1} />
+      )}
     </div>
   );
 }
